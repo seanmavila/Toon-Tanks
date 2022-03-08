7202b584733d0da1170809030238757b622f3958
Sean Avila <sean.avila94@gmail.com>
2022-03-07 19:07:46 -0600

    added tank and turret classes
    added basepawn class and a tank and turret class that inherit from basepawn
    
    added movement to tank subclass

---
+++ ToonTanks/Intermediate/Build/Win64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30136
+#include "C:/Users/seana/Game Repos/UnrealEngine/Toon-Tanks/ToonTanks/Intermediate/Build/Win64/ToonTanksEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ToonTanks/Source/ToonTanks/Tank.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ATank::ATank()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->SetupAttachment(SpringArm);
+}
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+}
+
+void ATank::Move(float Value)
+{
+	FVector DeltaLocation(0.f);
+
+	DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalOffset(DeltaLocation);
+}
